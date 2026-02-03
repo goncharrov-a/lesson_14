cf3ec1f31c0edae7c506fb4165ef32425076e19f
Goncharov Anton <gonchrv.x@icloud.com>
2026-02-03 23:33:23 +0300

    test: add clean AssertionError message in Allure

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,13 @@
 from selene import browser, have, be
+from selene.core.exceptions import TimeoutException
 
 
 class MainPage:
     LOGO = "[data-testid='header_logo']"
     DOWNLOAD_BUTTON = "[data-testid='download_rustore_button']"
     SEARCH_BUTTON = "[data-testid='header_search_button']"
+
+    HEADER_LINKS = "[data-testid='header_routeLink']"
 
     def open(self):
         browser.open("/")
@@ -33,4 +36,15 @@ class MainPage:
 
     def should_have_download_button(self):
         browser.element(self.DOWNLOAD_BUTTON).should(be.visible).should(be.clickable)
+        return self
+
+    def header_link(self, text: str):
+        return browser.all(self.HEADER_LINKS).element_by(have.exact_text(text))
+
+    def should_have_clickable_header_link(self, text: str):
+        try:
+            self.header_link(text).should(be.visible).should(be.clickable)
+        except TimeoutException as e:
+            # Обработка ошибки для примера
+            raise AssertionError(f"Пункт меню хедера '{text}' не найден/не кликабелен") from e
         return self
